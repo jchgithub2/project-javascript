00ea1a8b6653484ebe26c0459f96b028da185773
jchgithub2 <jchgithub2@users.noreply.github.com>
2023-02-20 23:34:36 -0500

    funcion js fibonacci!. Ejemplo: serie que suma de dos números anteriores hasta el numero indicado.

--- programa.js
+++ programa.js
@@ -1,1 +1,14 @@
-console.log("Hola, Bienvenidos al Repositorio de project-javascript")
+function fib(numbers){
+    let serie = [0, 1];
+    for (let i = 2; i <= numbers; i++){
+        serie.push(serie[i - 1] + serie[ i - 2]);
+    }
+  return [serie, serie[numbers]];
+}
+console.log("Hola, Bienvenidos al Repositorio de project-javascript");
+
+console.log("Serie completa: ",fib(15)[0]);
+console.log("Resultado de fib: ",fib(15)[1]);
+
+
+
